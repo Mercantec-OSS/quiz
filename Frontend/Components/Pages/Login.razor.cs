cdb110c89d118164c296c598228932b198f7a664
Alexander Verona Piilgaard <alex344b@edu.mercantec.dk>
2024-08-22 15:38:55 +0200

    making a login page, still alot of issues, but will be fixed soon

---
+++ Frontend/Components/Pages/Login.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Identity.Data;
+using System.Text;
+
+namespace Frontend.Components.Pages
+{
+    public class Login
+    {
+        public LoginRequest LoginRequest = new LoginRequest();
+
+        private HttpClient client = new HttpClient() { BaseAddress = new Uri("https://mercantec-quiz.onrender.com")};
+        
+
+
+        public async Task HandleLogin()
+        {
+            string json = System.Text.Json.JsonSerializer.Serialize(LoginRequest);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("api/Users/login", content);
+            if (response.IsSuccessStatusCode) Navigation.NavigateTo("/Home");
+        }
+    }
+}

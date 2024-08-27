c729f3d339337dbac456ce97becd93c7839114e5
Alexander Verona Piilgaard <alex344b@edu.mercantec.dk>
2024-08-27 15:51:02 +0200

    still null value on LoginRequest.Email and Password, needs to be fixed.

--- Frontend/Components/Pages/Login.razor.cs
+++ Frontend/Components/Pages/Login.razor.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity.Data;
 using System.Text;
 using API.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Frontend.Components.Pages
 {
@@ -15,10 +16,10 @@ namespace Frontend.Components.Pages
         
         public async Task HandleLogin()
         {
-
+            Console.WriteLine($"Email: {LoginRequest.Email}, Password: {LoginRequest.Password}");
             string json = System.Text.Json.JsonSerializer.Serialize(LoginRequest);
             var content = new StringContent(json, Encoding.UTF8, "application/json");
-            var response = await client.PostAsync("api/Users/login", content);
+            var response = await client.PostAsJsonAsync("api/Users/login", LoginRequest);
             if (response.IsSuccessStatusCode) Navigation.NavigateTo("/");
         }
     }

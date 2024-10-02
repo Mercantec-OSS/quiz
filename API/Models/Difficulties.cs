e4c50fe180c20257df68f964f344c142bd69f9fa
Alexander Verona Piilgaard <alex344b@edu.mercantec.dk>
2024-10-02 14:04:08 +0200

    made the changes to, difficulty, category and under category

--- API/Models/Difficulties.cs
+++ API/Models/Difficulties.cs
@@ -5,5 +5,8 @@
         [Key]
         public int ID { get; set; }
         public int Difficulty { get; set; }
+
+        //we need to have a difficultyName
+        //public string difficultyName { get; set; }
     }
 }

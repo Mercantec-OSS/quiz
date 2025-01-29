714413ea762a0d93015ffa6e1f843eee642f24f4
Jonas N jensen <102585691+JonasNJensen@users.noreply.github.com>
2025-01-29 09:50:38 +0100

    test for ap connect string

--- API/Controllers/TestController.cs
+++ API/Controllers/TestController.cs
@@ -9,5 +9,16 @@
         {
             return Ok("If you get this its working.");
         }
+
+        [HttpGet("Ldap")]
+        public IActionResult GetLdap() 
+        {
+            string connectionString = Environment.GetEnvironmentVariable("AD_CONNECTION_STRING") ?? "";
+            if (connectionString != "") 
+            { 
+            return Ok("the sting exist");
+            }
+            return Ok("string is missing");
+        }
     }
 }
